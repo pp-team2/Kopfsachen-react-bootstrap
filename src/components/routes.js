68aac6fc15ce629058074c413609d12b2a3ab73c
FuratH <89547877+FuratH@users.noreply.github.com>
2022-05-28 13:21:00 +0200

    Update routes.js

--- src/components/routes.js
+++ src/components/routes.js
@@ -2,19 +2,16 @@ import {React , useState, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from './navigator'
 import Tagebuch from './tagebuch'
-import  Header  from './header'
-import  Missing  from './missing'
-import  Wiki  from './wikiList'
+import Header from './header'
+import Missing from './missing'
+import Wiki from './wikiList'
 import Wikientry from './wikientry'
 import App from '../App'
 
 //https://www.flatuicolorpicker.com/colors/sauvignon/
 
-
-
 function GetWikiData(){
     const [wikii, setWiki] = useState([])
-
 
       useEffect(() => {
         fetch('http://127.0.0.1:4010/wiki/quiadfd')
@@ -29,7 +26,7 @@ function GetWikiData(){
 
 const Routes = () => {
 
-    const data = GetWikiData();
+const data = GetWikiData();
 
 const routess = [{
   path: ["/", "/home"],
@@ -58,56 +55,35 @@ const routess = [{
   color: "#eefcf5",
   text: "Komponente kommt bald!",
   img: "./logoBig.png"
-}
-]
-
-
-
-
-
-
+}]
 
     return (
     <Router>
       <Nav/>
       <Switch>
-
-
-        
         {
-            data.map(wikiEntry => {
-                console.log(`/wiki/${wikiEntry.id.replace(/\s+/g, '')}`)
-                return(
-                    <Route key={wikiEntry.id} path={`/wiki/${wikiEntry.id.replace(/\s+/g, '')}`}>
-                    
-                      <Header color="#f6efe9" text={wikiEntry.title} img=""/>
-                      <Wikientry object={wikiEntry} />
-                    </Route>
-                  )
-            })
+          data.map(wikiEntry => {
+            console.log(`/wiki/${wikiEntry.id.replace(/\s+/g, '')}`)
+            return(
+                <Route key={wikiEntry.id} path={`/wiki/${wikiEntry.id.replace(/\s+/g, '')}`}>
+                
+                  <Header color="#f6efe9" text={wikiEntry.title} img=""/>
+                  <Wikientry object={wikiEntry} />
+                </Route>
+              )
+          })
         }
 
-      {
-      /*wiki.flatMap(x => x.entries).map((wikiEntry, index) => {
-        console.log(wikiEntry)
-          return(
-            <Route key={index} path={`/wiki/${wikiEntry.title.replace(/\s+/g, '')}`}>
-              <Header color="#f6efe9" text={wikiEntry.title} img={wikiEntry.img}/>
-              <Wikientry title={wikiEntry.title} content={wikiEntry.content} />
-            </Route>
-          )
-        })
-    */}
-
-
-        {routess.map((route, index) => {
-          return(
-            <Route key={index} exact path={route.path}>
-              <Header color={route.color} text={route.text} img={route.img}/>
-              {route.component}
-            </Route>
-          )
-        })}
+        {
+          routess.map((route, index) => {
+            return(
+              <Route key={index} exact path={route.path}>
+                <Header color={route.color} text={route.text} img={route.img}/>
+                {route.component}
+              </Route>
+            )
+          })
+        }
 
       </Switch>
     </Router>
