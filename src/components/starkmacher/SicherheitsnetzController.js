52c0a16692d67173e54e397ffa885a552e116dd9
Leonhardt2 <l-hollatz@web.de>
2022-06-30 17:28:19 +0200

    GET und POST Request Überarbeitung

--- src/components/starkmacher/SicherheitsnetzController.js
+++ src/components/starkmacher/SicherheitsnetzController.js
@@ -12,49 +12,53 @@ export default class SicherheitsnetzController extends React.Component {
         feedback: [{timestamp: '', comment: '', itHelped: false}]}];
         let lastID = 0;
 
-        // Lädt die abgespeicherten Aktivitäten
-        fetch('http://127.0.0.1:4010/safetyNet')
-            .then(response => response.json())
-            .then(data => {
-                data.forEach(line => {
-                    // Die Symboltypen in die source-Adresse übersetzen
-                    let imgSrc;
-                    switch (line.type) {
-                        case 'friends':
-                            imgSrc = "/personen.png";
-                            break;
-                        case 'sport':
-                            imgSrc = "/kreativ.png";
-                            break;
-                        case 'pet':
-                            imgSrc = "/tier.png";
-                            break;
-                        case 'music':
-                            imgSrc = "/kreativ.png";
-                            break;
-                        case 'personality':
-                            imgSrc = "/personen.png";
-                            break;
-                        case 'situationControl':
-                            imgSrc = "/situationskontrolle.png";
-                            break;
-                        case 'relaxation':
-                            imgSrc = "/tagebuch.jpg";
-                            break;
-                        case 'other':
-                            imgSrc = "/sonstiges.png";
-                            break;
-                        default:
-                            imgSrc = "/sonstiges.png";
-                    }
+        // Lädt die abgespeicherten Aktivitäten (synchron)
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET",'http://127.0.0.1:4010/safetyNet', false);
+        xhr.send();
+        let data = JSON.parse(xhr.responseText);
 
-                    activities.push({id: lastID+1, text: line.name, picture: imgSrc, placeID: 'svg_'+(lastID+1), strategies: line.strategies, feedback: line.feedback});
-                    lastID = activities[activities.length-1].id;
-                });
-            });
+        // Transformiert die Daten in das Format welches hier verwendet wird
+        data.forEach(line => {
+            // Die Symboltypen in die source-Adresse übersetzen
+            let imgSrc;
+            switch (line.type) {
+                case 'friends':
+                    imgSrc = "/personen.png";
+                    break;
+                case 'sport':
+                    imgSrc = "/kreativ.png";
+                    break;
+                case 'pet':
+                    imgSrc = "/tier.png";
+                    break;
+                case 'music':
+                    imgSrc = "/kreativ.png";
+                    break;
+                case 'personality':
+                    imgSrc = "/personen.png";
+                    break;
+                case 'situationControl':
+                    imgSrc = "/situationskontrolle.png";
+                    break;
+                case 'relaxation':
+                    imgSrc = "/tagebuch.jpg";
+                    break;
+                case 'other':
+                    imgSrc = "/sonstiges.png";
+                    break;
+                default:
+                    imgSrc = "/sonstiges.png";
+            }
 
+            activities.push({id: lastID+1, text: line.name, picture: imgSrc, placeID: 'svg_'+(lastID+1), strategies: line.strategies, feedback: line.feedback});
+            lastID = activities[activities.length-1].id;
+        });
+ 
+        // Setzt den State
         this.state = {site: null, clickID: 0, 
-            activities: activities};
+            activities: activities}; 
+
         this.addNewActivity = this.addNewActivity.bind(this);
         this.selectNewActivity = this.selectNewActivity.bind(this);
         this.saveActivity = this.saveActivity.bind(this);
@@ -64,23 +68,59 @@ export default class SicherheitsnetzController extends React.Component {
         this.deleteActivity = this.deleteActivity.bind(this);
     }
 
+    // Sendet eine Aktivität an die API
     postActivity(activity) {
         // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-        // Example POST method implementation:
-        fetch('http://127.0.0.1:4010/safetyNet', {
-            method: 'POST', // or 'PUT'
+        fetch('http://127.0.0.1:4010/safetyNet/172', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(activity),
         })
-        .then(response => response.json())
         .then(data => {
             console.log('Success:', data);
         })
         .catch((error) => {
             console.error('Error:', error);
         });
+    }
+
+    // Transformiert das Format der Aktivitätsobjekte in das Format für die API
+    transformActivitiy(activity) {
+        let name = activity.text;
+        let type;
+        switch (activity.picture) {
+            case '/personen.png':
+                type = "friends";
+                break;
+            case '/kreativ.png':
+                type = "sport";
+                break;
+            case '/tier.png':
+                type = "pet";
+                break;
+            case '/kreativ.png':
+                type = "music";
+                break;
+            case '/personen.png':
+                type = "personality";
+                break;
+            case '/situationskontrolle.png':
+                type = "situationControl";
+                break;
+            case '/tagebuch.jpg':
+                type = "relaxation";
+                break;
+            case '/sonstiges.png':
+                type = "other";
+                break;
+            default:
+                type = "other";
+        }
+        let strategies = activity.strategies;
+        let feedback = activity.feedback;
+        return {name: name, type: type, strategies: strategies, feedback, feedback};
     }
 
     saveActivity(text, picture, placeID) {
@@ -113,7 +153,9 @@ export default class SicherheitsnetzController extends React.Component {
         this.selectNewActivity();
     }
 
+    // Löscht eine Aktivität aus dem Array im State
     deleteActivity(id) {
+        this.postActivity(this.transformActivitiy(this.state.activities.filter(line => line.id === +id)[0]));
         let activities = this.state.activities;
         activities = activities.filter(line => line.id !== +id);
         console.log(activities);
@@ -153,7 +195,7 @@ export default class SicherheitsnetzController extends React.Component {
                 break;
             case 3: toShow = <Feedback></Feedback>
                 break;
-            default: toShow = <Sicherheitsnetz addNewActivity={this.addNewActivity} activities={this.state.activities}></Sicherheitsnetz>
+            default: toShow = <Sicherheitsnetz addNewActivity={this.addNewActivity} activities={this.state.activities} deleteActivity={this.deleteActivity} commentActivity={this.commentActivity}></Sicherheitsnetz>
         }
         return (
             <div>
