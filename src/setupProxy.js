81171b94f34a3b065ca99524eef3adeb49dd6695
FuratH <hamdanfurat@gmail.com>
2022-07-16 15:08:39 +0200

    change proxy url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,28 +4,28 @@ module.exports = function(app) {
 
     app.use("/self-service",
         createProxyMiddleware({
-            target: 'https://auth.api.dev.mindtastic.lol',
+            target: 'https://auth.api.live.mindtastic.lol',
             changeOrigin: true,
         })
     );
 
     app.use("/sessions",
         createProxyMiddleware({
-            target: 'https://auth.api.dev.mindtastic.lol',
+            target: 'https://auth.api.live.mindtastic.lol',
             changeOrigin: true,
         })
     );
 
     app.use("/wiki",
         createProxyMiddleware({
-            target: 'https://wiki.api.dev.mindtastic.lol',
+            target: 'https://wiki.api.live.mindtastic.lol',
             changeOrigin: true,
         })
     );
 
     app.use("/user",
         createProxyMiddleware({
-            target: 'https://users.api.dev.mindtastic.lol',
+            target: 'https://users.api.live.mindtastic.lol',
             changeOrigin: true,
         })
     );
